fd4cd643f4bfd3fcc10d0ef8509bec201f6940e0
Joshua Lee <me@jolly.sg>
2017-03-21 12:46:32 +0800

    starting to focus on editing ICMP packets without any IP header options

--- netfilter.c
+++ netfilter.c
@@ -8,6 +8,7 @@
 #include <linux/ip.h>
 #include <linux/tcp.h>
 #include <linux/skbuff.h>
+#include <net/ip.h>
 
 #define NIPQUAD(addr) \
     ((unsigned char *)&addr)[0], \
@@ -21,6 +22,8 @@ struct tcphdr *tcp_header;
 struct sk_buff *sock_buff;
 unsigned int sport, dport;
 
+void print_ip_header_options(struct sk_buff **skb, struct iphdr *iph);
+
 unsigned int hook_func(unsigned int hooknum,
                        struct sk_buff **skb,
                        const struct net_device *in,
@@ -29,7 +32,7 @@ unsigned int hook_func(unsigned int hooknum,
 {
     //NOTE: Feel free to uncomment printks! If you are using Vagrant and SSH
      //      too many printk's will flood your logs.
-    printk(KERN_INFO "=== BEGIN PACKET ===\n");
+    // printk(KERN_INFO "=== BEGIN PACKET ===\n");
 
     sock_buff = skb;
 
@@ -40,34 +43,34 @@ unsigned int hook_func(unsigned int hooknum,
     iph = (struct iphdr *)skb_network_header(sock_buff);
 
     if (!iph) {
-        printk(KERN_INFO "Packet has no IP header\n");
+        // printk(KERN_INFO "Packet has no IP header\n");
         // return NF_ACCEPT;
     } else {
-        printk(KERN_INFO "Packet has IP header\n");
+        // printk(KERN_INFO "Packet has IP header\n");
+        // printk(KERN_INFO "Packet size: %d\n", iph->tot_len * 4);
+        // printk(KERN_INFO "IP header size: %d\n", iph->ihl * 4);
         if (iph->ihl * 4 > sizeof(struct iphdr)) {
-            // based on: https://github.com/torvalds/linux/blob/6939c33a757bd006c5e0b8b5fd429fc587a4d0f4/net/netfilter/xt_LOG.c
-            printk(KERN_INFO "Packet has IP header options\n");
-            const unsigned char *op;
-            unsigned char _opt[4 * 15 - sizeof(struct iphdr)];
-            unsigned int i, optsize;
-
-            optsize = iph->ihl * 4 - sizeof(struct iphdr);
-            op = skb_header_pointer(skb, sizeof(struct iphdr), optsize, _opt);
-            if (op == NULL) {
-                printk(KERN_INFO "NULL IP options\n");
-            } else {
-                /* Max length: 127 "OPT (" 15*4*2chars ") " */
-                printk(KERN_INFO "IP options: (");
-                for (i = 0; i < optsize; i++) {
-                    printk(KERN_CONT "%02X", op[i]);
-                }
-                printk(KERN_CONT ")\n");
-            }
+            // printk(KERN_INFO "Packet has IP header options\n");
+            // print_ip_header_options(skb, iph);
         } else {
-            printk(KERN_INFO "Packet has no IP header options\n");
-        }
-        if(iph->protocol==IPPROTO_ICMP) {
-            printk(KERN_INFO "ICMP packet detected\n");
+            // printk(KERN_INFO "Packet has no IP header options\n");
+            if(iph->protocol==IPPROTO_ICMP) {
+                printk(KERN_INFO "=== BEGIN PACKET ===\n");
+                printk(KERN_INFO "ICMP packet with no IP header options detected\n");
+                printk(KERN_INFO "Packet size: %d\n", iph->tot_len * 4);
+                printk(KERN_INFO "IP header size: %d\n", iph->ihl * 4);
+
+                // add stuff here to mangle IP header
+                
+
+                // Calculation of IP header checksum
+                iph->check = 0;
+                ip_send_check(iph);
+
+                printk(KERN_INFO "New IP header size: %d\n", iph->ihl * 4);
+                printk(KERN_INFO "New Packet size: %d\n", iph->tot_len * 4);
+                printk(KERN_INFO "=== END PACKET ===\n");
+            }
         }
     }
 
@@ -93,9 +96,29 @@ unsigned int hook_func(unsigned int hooknum,
     //if(in) { printk(KERN_INFO "in->name:  %s\n", in->name); }
     //if(out) { printk(KERN_INFO "out->name: %s\n", out->name); }
     
-    printk(KERN_INFO "=== END PACKET ===\n");
+    // printk(KERN_INFO "=== END PACKET ===\n");
 
     return NF_ACCEPT;
+}
+
+void print_ip_header_options(struct sk_buff **skb, struct iphdr *iph) {
+    // based on: https://github.com/torvalds/linux/blob/6939c33a757bd006c5e0b8b5fd429fc587a4d0f4/net/netfilter/xt_LOG.c
+    const unsigned char *op;
+    unsigned char _opt[4 * 15 - sizeof(struct iphdr)];
+    unsigned int i, optsize;
+    optsize = iph->ihl * 4 - sizeof(struct iphdr);
+    printk(KERN_INFO "IP header options size: %d\n", optsize);
+    op = skb_header_pointer(skb, sizeof(struct iphdr), optsize, _opt);
+    if (op == NULL) {
+        printk(KERN_INFO "NULL IP options\n");
+    } else {
+        /* Max length: 127 "OPT (" 15*4*2chars ") " */
+        printk(KERN_INFO "IP options: (");
+        for (i = 0; i < optsize; i++) {
+            printk(KERN_CONT "%02X", op[i]);
+        }
+        printk(KERN_CONT ")\n");
+    }
 }
 
 static int __init initialize(void) {
